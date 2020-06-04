a9896b339d0735e355c331cd73c1fe413d862ed9
dongmin <simon-dong@qq.com>
2020-06-04 18:29:05 +0800

    删除无关文件

--- src/extension.js
+++ src/extension.js
@@ -1,18 +1,4 @@
 
-/**
- * 插件被激活时触发，所有代码总入口
- * @param {*} context 插件上下文
- */
 exports.activate = function(context) {
-  console.log('恭喜，您的扩展“one-todo”已被激活！');
-  require('./homepage')(context); // 首页
-  require('./helloworld')(context); // hello word
-  require('./todo')(context); // TODO
+  require('./todo')(context);
 };
-
-/**
- * 插件被释放时触发
- */
-exports.deactivate = function() {
-  console.log('您的扩展“one-todo”已被释放！');
-}
